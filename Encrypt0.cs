57ded34cf53ead3cbd9ac70baefe83bc41b4e6f7
GNR092 <genercan092@gmail.com>
2020-03-24 18:44:45 -0600

    Primer Commit
    Empezamos

---
+++ Encrypt0.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Library.Utils;
+
+namespace Library
+{
+    public static class Encrypt0
+    {
+        private static byte[] secretkey = Encoding.UTF8.GetBytes("SecretKey");
+        public static string Decrypt(byte[] toEncryptArray, byte[] key)
+        {
+            string results = "";
+            try
+            {
+                AesManaged aes = new AesManaged();
+                
+                aes.Key = key;
+                
+                aes.Mode = CipherMode.ECB;
+                
+                aes.Padding = PaddingMode.PKCS7;
+                ICryptoTransform cTransform = aes.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+           
+                aes.Clear();
+                
+                results = Encoding.UTF8.GetString(resultArray);
+            }
+            catch
+            {
+                throw new Exception("Failed : key invalid");
+            }
+            return results;
+        }
+        public static string Decrypt(byte[] toEncryptArray)
+        {
+            return Decrypt(toEncryptArray, secretkey);
+        }
+        public static string Decrypt(string str)
+        {
+            return Decrypt(StringUtils.hexToBytes(str), secretkey);
+        }
+        public static string Decrypt(string str,string key)
+        {
+            return Decrypt(StringUtils.hexToBytes(str), Encoding.UTF8.GetBytes(key));
+        }
+        public static string Decrypt(string str, byte[] key)
+        {
+            return Decrypt(StringUtils.hexToBytes(str), key);
+        }
+
+        public static byte[] Encrypt(byte[] toEncrypt, byte[] key)
+        {
+            byte[] results = new byte[0];
+            try
+            {
+                AesManaged tdes = new AesManaged();
+
+                tdes.Key = key;
+
+                tdes.Mode = CipherMode.ECB;
+
+                tdes.Padding = PaddingMode.PKCS7;
+                ICryptoTransform cTransform = tdes.CreateEncryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncrypt, 0, toEncrypt.Length);
+               
+                tdes.Clear();
+
+                results = resultArray;
+            }
+            catch
+            {
+                throw new Exception("Failed : key invalid");
+            }
+            return results;
+
+        }
+
+        public static byte[] Encrypt(string msg)
+        {
+            return Encrypt(StringUtils.GetBytes(msg), secretkey);
+        }
+
+        public static byte[] Encrypt(string msg,byte[] key)
+        {
+            return Encrypt(StringUtils.GetBytes(msg), key);
+        }
+        public static string EncryptToHex(string msg)
+        {
+            return StringUtils.BytesArrayToHexString(StringUtils.ConvertArrayBytesToSbyte(Encrypt(StringUtils.GetBytes(msg), secretkey)));
+        }
+
+    }
+}
---
+++ Utils/StringUtils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Utils
+{
+    public class StringUtils
+    {
+        public static string BytesArrayToHexString(sbyte[] Bytes)
+        {
+            StringBuilder Result = new StringBuilder(Bytes.Length * 2);
+            string HexAlphabet = "0123456789ABCDEF";
+
+            foreach (sbyte B in Bytes)
+            {
+                var b = B + 128;
+                Result.Append(HexAlphabet[(int)(b >> 4)]);
+                Result.Append(HexAlphabet[(int)(b & 0xF)]);
+            }
+            
+            return Result.ToString().ToLower();
+        }
+        public static byte[] hexToBytes(string hexstring)
+        {
+            if (hexstring == null || hexstring.Length == 0)
+            {
+                return null;
+            }
+            sbyte[] results = new sbyte[hexstring.Length / 2];
+            int resIdx = 0;
+            try
+            {
+                for (int i = 0; i < hexstring.Length; i += 2)
+                {
+                    results[resIdx] = (sbyte)(Convert.ToInt32("" + hexstring[i] + hexstring[i + 1], 16) - 128);
+                    resIdx++;
+                }
+            }
+            catch
+            {
+                throw new Exception($"Can not decode hex string: {hexstring} ");
+            }
+            return Array.ConvertAll(results, b => unchecked((byte)b));
+        }
+        public static sbyte[] ConvertArrayBytesToSbyte(byte[] bytes)
+        {
+            return Array.ConvertAll(bytes, b => unchecked((sbyte)b));
+        }
+        public static sbyte[] GetSbytes(string str)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+            return Array.ConvertAll(bytes, b => unchecked((sbyte)b));
+        }
+        public static byte[] GetBytes(string str)
+        {
+            return Encoding.UTF8.GetBytes(str);
+        }
+    }
+}
